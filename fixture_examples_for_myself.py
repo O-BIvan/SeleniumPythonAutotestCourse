de8af8eb61dcf0cc19368e5f7d6855c6b89b9adb
Ivan B <70215486+O-BIvan@users.noreply.github.com>
2020-08-27 00:31:43 +0300

    Add autouse explanation link

--- fixture_examples_for_myself.py
+++ fixture_examples_for_myself.py
@@ -5,6 +5,7 @@ import pytest
 # Фикстура, в данном случае, весь код после @pytest.fixture и до следующей @pytest.fixture
 # н., def suite_data... ...Suite teardown")
 # yield - генератор (см. lesson3_step5_fixture_example4.py)
+# autouse - см. lesson3_step6_fixture_example5.py
 
 
 @pytest.fixture(scope='class', autouse=True)  # "Настройки" для тест-СЦЕНАРИЯ - запускаются для всех тестов в наборе
